a7dbf69db0983bf9d7da2161dea2d9c09b582fab
Md. Monjurul Haque <himel452@gmail.com>
2026-02-07 02:25:32 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ const ASSETS = [
     '/manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
     'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&family=Rajdhani:wght@400;600&display=swap'
+    vinchi.pdf
     // এখানে আপনার পিডিএফ ফাইলের নাম যোগ করতে পারেন অফলাইনে পড়ার জন্য
     // '/1.pdf' 
     সফলতার গাইডলাইন
